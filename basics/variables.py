3a1b103ffc98ab6bc60fd2ccf0016326ee5fe6a5
Neil Weightman <python-dev-training@proton.me>
2025-01-22 11:02:44 +0000

    Added variables

---
+++ basics/variables.py
@@ -0,0 +1,17 @@
+computer_name = "Server A"
+print(computer_name)
+computer_name = "User Workstation 12"
+print(computer_name)
+computer_id = 1234  # this is a comment
+performance_index = 12.85
+print(computer_id, performance_index)
+print(computer_id)
+computer_price = 850.00
+number_of_computers = 13
+print(computer_price * number_of_computers)
+performance_index = 90
+print(type(performance_index))
+
+# print()
+operating_system = input("Please enter this computer's operating system:")
+print("Operating system:", operating_system)
