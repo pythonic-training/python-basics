a9cf122948d743f0502f2c6909aed3c24205ac68
Neil Weightman <python-dev-training@proton.me>
2025-01-22 11:22:47 +0000

    Added data_types.py

---
+++ basics/data_types.py
@@ -0,0 +1,9 @@
+computer_id = 1234  # int
+print(type(computer_id))
+performance_index = 123.45  # 1.23e6 is 1.23 million float
+print(type(performance_index))
+is_assigned_to_user = True  # bool can also be False
+print(type(is_assigned_to_user))
+computer_name = 'Main Router'
+print(type(computer_name))
+# list set dict tuple
